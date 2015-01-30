c16d515b47a1447225706bcf4526196777b59605
bauner <bauner@gmx.de>
2015-01-30 17:48:50 +0100

    fixed media_profiles.xml

--- common.mk
+++ common.mk
@@ -118,7 +118,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
 	$(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
 	$(COMMON_PATH)/lib/libjni_mosaic.so:system/lib/libjni_mosaic.so \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
