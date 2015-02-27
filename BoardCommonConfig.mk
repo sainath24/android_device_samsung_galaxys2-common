8ccfb185dbec735e682086c182b261bd70c49a3a
bauner <bauner@gmx.de>
2015-02-27 15:48:23 +0100

    cleanup

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -63,14 +63,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0 androidboot.selinux=permissive
 
-# Enable dex-preoptimization to speed up first boot sequence
-# ifeq ($(HOST_OS),linux)
-# 	ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-# 		WITH_DEXPREOPT := true
-# 	endif
-# endif
-# DONT_DEXPREOPT_PREBUILTS := true
-
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
