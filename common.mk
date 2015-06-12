38390b06fd1cc629ae142e905de1ea8675512766
bauner <bauner@gmx.de>
2015-06-12 14:30:08 +0200

    wifi config update

--- common.mk
+++ common.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(COMMON_PATH)/configs/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
 PRODUCT_PROPERTY_OVERRIDES += \
