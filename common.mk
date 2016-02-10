55bdbccd6d6ac2baaec53ceaf2a6123a6943ac40
bauner <bauner@gmx.de>
2016-02-10 19:29:54 +0100

    new netflix hack
    remove adaptive playback from google avc codec to force netflix using hw
    codec

--- common.mk
+++ common.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # Netflix hack
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/98netflix:system/etc/init.d/98netflix
+	$(COMMON_PATH)/configs/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video.xml
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -115,7 +115,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+# we overlay this file (see netflix hack above)
+#   frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
