7baab3a31a7cf1b82da1d62a62e69cdcc41c7a52
sainath <sainathprasanna@gmail.com>
2016-05-04 13:26:37 +0530

    phone-xhdpi-1024-dalvik-heap

--- common.mk
+++ common.mk
@@ -210,8 +210,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
+#$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Custom dalvik.vm values
 # PRODUCT_PROPERTY_OVERRIDES += \
